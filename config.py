005d1d0129159e4413bea124722bdb63f3a9b9ae
CloudSky <yijianbaiyuntian@gmail.com>
2016-09-22 21:49:47 +0800

    Revert "1.0"
    This reverts commit 15c4e15ed5a5c9962016f53d23e2a5d728e4543c.

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-
-rootdir = sys.path[0]
-config_file = os.path.join(rootdir,'hosts.ini')
-
-CSRF_ENABLED = True
--- web.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from app import app
-
-__version = 0.5
- 
-if  __name__ == '__main__':
-    #app.debug=True
-    app.run(debug=True,host="192.168.31.57",port=8080)
